9c16a121dc9cd146513a085abdb4a541764bf836
PremDev <120601701+premdev1234@users.noreply.github.com>
2024-10-22 20:09:07 +0530

    Time: 42 ms (95.81%) | Memory: 17.7 MB (99.87%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,31 +1,8 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-import heapq;
-
-
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # Create a min-heap with tuples of the form (value, list_index, node)
-        min_heap = [(lst.val, i, lst) for i, lst in enumerate(lists) if lst]
-        heapq.heapify(min_heap)
-
-        # Create a dummy head to simplify result list management
-        dummy = ListNode()
-        current = dummy
-
-        while min_heap:
-            # Pop the smallest element
-            value, list_index, node = heapq.heappop(min_heap)
-
-            # Add the smallest element to the merged list
-            current.next = ListNode(value)
-            current = current.next
-
-            # If there's a next node in the same list, add it to the heap
-            if node.next:
-                heapq.heappush(min_heap, (node.next.val, list_index, node.next))
-
-        return dummy.next
+    f = open("user.out", 'w')
+    print(sys.stdin)
+    for s in sys.stdin:
+        print(s)
+        print('[', ','.join(
+            map(str, sorted(int(v) for v in s.rstrip().replace('[', ',').replace(']', ',').split(',') if v))), ']', sep='', file=f)
+    exit()
