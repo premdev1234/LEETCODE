3e282ad79292139b67329913de01065a029b3dcf
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-02-06 14:39:32 +0530

    Time: 1 ms (76.51%) | Memory: 60.1 MB (31.43%) - LeetSync

---
+++ 1659-get-the-maximum-score/get-the-maximum-score.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums1, vector<int>& nums2) {
+        ios::sync_with_stdio(false);
+        
+
+        const int mod = 1e9 + 7;
+        long long sum1 = 0, sum2 = 0  , result =0 ;
+        int i = 0, j = 0;
+        int n1 = nums1.size(), n2 = nums2.size();
+
+        while (i < n1 && j < n2){
+            if (nums1[i] < nums2[j])  sum1 += nums1[i++];
+            else if (nums1[i] > nums2[j])  sum2 += nums2[j++];
+            else {
+                result += max(sum1, sum2) + nums1[i]; 
+                sum1 = 0 , sum2 = 0;
+                i++, j++;
+            }
+        }
+        while(i < n1) sum1 += nums1[i++];
+        while(j < n2) sum2 += nums2[j++];
+        result += max(sum1, sum2);
+        return result % mod;
+    }
+};
