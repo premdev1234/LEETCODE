c8c8785e7825f378c0419d80d1192f1ce068a5b4
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-02 11:49:15 +0530

    Time: 83 ms (26.89%) | Memory: 146.2 MB (93.12%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mod_index;
+        mod_index[0] = -1;  // handle case where subarray starts at index 0
+
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            int mod = (k == 0) ? sum : sum % k;
+
+            if (mod_index.count(mod)) {
+                if (i - mod_index[mod] >= 2) return true;
+            } else {
+                mod_index[mod] = i;  // only store the first occurrence
+            }
+        }
+        return false;
+    }
+};
