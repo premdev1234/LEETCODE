2f9a5e836695cf31c6d5b4d28f648ccff7a41465
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-02 07:27:00 +0530

    Time: 190 ms (51.54%) | Memory: 12.6 MB (66.85%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        vector<bool> isprime(n, true); 
+        isprime[0] = isprime[1] = false;
+
+        for (int i = 2; i * i < n; ++i) {
+            if (isprime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isprime[j] = false;
+                }
+            }
+        }
+
+        return count(isprime.begin(), isprime.end(), true);
+    }
+};
