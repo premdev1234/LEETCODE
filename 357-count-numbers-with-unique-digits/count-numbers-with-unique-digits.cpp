735fc41d4963e7b02089cc600bffcb10bd6a481c
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-03 16:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (39.81%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
-        int res[9] = {1,10,91,739,5275,32491,168571,712891,2345851};
-        return res[n];
+        if(n == 0) return 1;
+        int ans = 10, prod = 9, cap = 9;
+        for(int i = 2; i <= n && cap > 0; i++) {
+            prod *= cap;
+            ans += prod;
+            cap--;
+        }
+        return ans;
     }
 };
