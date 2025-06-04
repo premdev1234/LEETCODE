47aa197090c08e246bba569ef2ceaed45d0c8af7
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-04 17:19:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (69.67%) - LeetSync

--- 467-unique-substrings-in-wraparound-string/unique-substrings-in-wraparound-string.cpp
+++ 467-unique-substrings-in-wraparound-string/unique-substrings-in-wraparound-string.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int findSubstringInWraproundString(string s) {
-        if (s.empty()) return 0;
-
-        int dp[26] = {0};  // Initialize all to 0
-        int k = 0;
-
-        for (int i = 0; i < (int)s.size(); ++i) {
-            if (i > 0 && ((s[i] - s[i - 1] + 26) % 26 == 1))k++;
-            else k = 1;
-            int index = s[i] - 'a';
-            if (dp[index] < k) dp[index] = k;
+        vector<int> dp(26,0);
+        int k= 0;
+        for(int i= 0; i< s.length();i++){
+            if(i>0 && (s[i]-s[i-1] + 26)%26 == 1) k++;
+            else k= 1;
+            dp[s[i]-'a']= max(dp[s[i]-'a'],k);
+            
         }
-
-        int result = 0;
-        for (int i = 0; i < 26; ++i) result += dp[i];
-
-        return result;
+        int ans= accumulate(begin(dp),end(dp),0);
+        return ans;
+        
     }
 };
