ade1c171c8426c86b1212924e5e13ac7c6e8ea24
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-04 09:27:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (97.09%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
+    void dfs(int city, vector<vector<int>>& isConnected, bitset<201>& visited) {
+        visited[city] = true;
+        for (int neighbor = 0; neighbor < isConnected.size(); ++neighbor) {
+            if (isConnected[city][neighbor] && !visited[neighbor]) {
+                dfs(neighbor, isConnected, visited);
+            }
+        }
+    }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> parent(n), rank(n, 0);
-        iota(parent.begin(), parent.end(), 0); // parent[i] = i
+        bitset<201> visited; 
 
-        function<int(int)> find = [&](int x) {
-            return parent[x] == x ? x : parent[x] = find(parent[x]);
-        };
-
-        auto unite = [&](int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX == rootY) return;
-            if (rank[rootX] < rank[rootY]) parent[rootX] = rootY;
-            else if (rank[rootX] > rank[rootY]) parent[rootY] = rootX;
-            else {
-                parent[rootY] = rootX;
-                rank[rootX]++;
+        int provinceCount = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                dfs(i, isConnected, visited);
+                provinceCount++;
             }
-        };
-
-        // Merge connected cities
-        for (int i = 0; i < n; ++i)
-            for (int j = i + 1; j < n; ++j)
-                if (isConnected[i][j])
-                    unite(i, j);
-
-        // Count unique roots
-        unordered_set<int> provinces;
-        for (int i = 0; i < n; ++i)
-            provinces.insert(find(i));
-
-        return provinces.size();
+        }
+        return provinceCount;
     }
 };
