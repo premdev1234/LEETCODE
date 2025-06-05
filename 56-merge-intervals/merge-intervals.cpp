0418d07fc1159f926c035fa614493f713070aa5a
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-05 12:40:57 +0530

    Time: 8 ms (29.02%) | Memory: 23.9 MB (59.32%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,12 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end()); 
-        vector<vector<int>> merged;
-        for (auto& interval : intervals) {
-            if (merged.empty() || merged.back()[1] < interval[0]) merged.push_back(interval);
-            else merged.back()[1] = max(merged.back()[1], interval[1]);
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> result;
+        vector<int> interval;
+        int last;
+
+        for(int i = 0; i < intervals.size(); i++)
+        {
+            if(i == 0)
+            {
+                interval.push_back(intervals[i][0]);
+                last = intervals[i][1];
+                continue;
+            }
+
+            if(intervals[i][0] <= last)
+            {
+                last = max(intervals[i][1],last);
+            }
+            else
+            {
+                interval.push_back(last);
+                result.push_back(interval);
+                interval.clear();
+                interval.push_back(intervals[i][0]);
+                last = max(intervals[i][1],last);
+
+            }
         }
-        return merged;
+
+        interval.push_back(last);
+        result.push_back(interval);
+
+        return result;
+        
     }
 };
