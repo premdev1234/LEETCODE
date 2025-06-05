ed0f464fc4db0f2241523df9418fbdaf3fd01259
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-05 16:30:07 +0530

    Time: 146 ms (37.31%) | Memory: 133.8 MB (64.40%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.cpp
+++ 1962-single-threaded-cpu/single-threaded-cpu.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         int n = tasks.size();
-        vector<array<int, 3>> indexedTasks; // {enqueue, processing, index}
         for (int i = 0; i < n; ++i) {
-            indexedTasks.push_back({tasks[i][0], tasks[i][1], i});
+            tasks[i].push_back(i);
         }
 
         // Step 1: Sort by enqueue time
-        sort(indexedTasks.begin(), indexedTasks.end());
+        sort(tasks.begin(), tasks.end());
 
         // Step 2: Min-heap based on processing time, then index // {processing time, index}
         priority_queue<pair<int, int>, vector<pair<int,int>>, greater<>> pq;
@@ -19,8 +18,8 @@ public:
 
         while (i < n || !pq.empty()) {
             // Load all tasks available at current time
-            while (i < n && indexedTasks[i][0] <= time) {
-                pq.emplace(indexedTasks[i][1], indexedTasks[i][2]);
+            while (i < n && tasks[i][0] <= time) {
+                pq.emplace(tasks[i][1], tasks[i][2]);
                 ++i;
             }
 
@@ -30,7 +29,7 @@ public:
                 result.push_back(idx);
             } else {
                 // No task available, jump to next task arrival
-                time = indexedTasks[i][0];
+                time = tasks[i][0];
             }
         }
 
