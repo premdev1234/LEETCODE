917464390447001428a4efc559f22dc4869e5124
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-06 16:37:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (83.78%) - LeetSync

---
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dfs(TreeNode* root, int minval, int maxval) {
+        if (!root) return abs(maxval - minval);
+        
+        minval = min(minval, root->val);
+        maxval = max(maxval, root->val);
+        
+        int left = dfs(root->left, minval, maxval);
+        int right = dfs(root->right, minval, maxval);
+        
+        return max(left, right);
+    }
+
+    int maxAncestorDiff(TreeNode* root) {
+        return dfs(root, root->val, root->val);
+    }
+};
