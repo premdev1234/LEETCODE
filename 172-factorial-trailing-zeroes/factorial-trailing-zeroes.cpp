1c8bf6c1c7a3b4a04548e731b014e396475081c9
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-09 12:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (16.03%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    int exponent(int n, int prime) {
+    int trailingZeroes(int n) {
         int exp = 0;
-        while (n >= prime) {
-            n /= prime;
+        while (n >= 5) {
+            n /= 5;
             exp += n;
         }
         return exp;
-    }
-
-    int trailingZeroes(int n) {
-        return exponent(n, 5);  // since 2s are more abundant than 5s
     }
 };
