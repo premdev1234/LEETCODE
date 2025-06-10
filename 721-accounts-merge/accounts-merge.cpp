ad5da26e65a8fc1362cebee26124a0d97ec62db9
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-10 17:19:53 +0530

    Time: 23 ms (93.15%) | Memory: 36.9 MB (96.52%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,11 +1,10 @@
 struct DSU {
     vector<int> parent, size;
     DSU(int n) : parent(n), size(n, 1) {
-        iota(parent.begin(),parent.end(), 0);
+        iota(parent.begin(), parent.end(), 0);
     }
     int find(int x) {
-        if (x != parent[x]) parent[x] = find(parent[x]);
-        return parent[x];
+        return parent[x] == x ? x : parent[x] = find(parent[x]);
     }
     bool unite(int a, int b) {
         a = find(a); b = find(b);
@@ -16,32 +15,41 @@ struct DSU {
         return true;
     }
 };
+
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n =  accounts.size();
+        int n = accounts.size();
         DSU dsu(n);
-        unordered_map<string, int> emailToAccount;
+        unordered_map<string, int> emailToId;
+
+        // Union by email occurrence
         for (int i = 0; i < n; ++i) {
             for (int j = 1; j < accounts[i].size(); ++j) {
-                const string& email = accounts[i][j];
-                if (emailToAccount.find(email) != emailToAccount.end()) dsu.unite(i, emailToAccount[email]);
-                else emailToAccount[email] = i;
+                string& email = accounts[i][j];
+                if (!emailToId.count(email))
+                    emailToId[email] = i;
+                else
+                    dsu.unite(i, emailToId[email]);
             }
         }
-        unordered_map<int, set<string>> mergedEmails;
-        for (int i = 0; i < n; ++i) {
-            int root = dsu.find(i);
-            for (int j = 1; j < accounts[i].size(); ++j) mergedEmails[root].insert(accounts[i][j]);
+
+        // Map from root to list of emails (use vector instead of set)
+        unordered_map<int, vector<string>> rootToEmails;
+        for (auto& [email, id] : emailToId) {
+            int root = dsu.find(id);
+            rootToEmails[root].push_back(email);
         }
-        vector<vector<string>> result;
-        for (auto& [root, emails] : mergedEmails) {
-            vector<string> account;
-            account.push_back(accounts[root][0]); // Add name
-            account.insert(account.end(), emails.begin(), emails.end()); // Add sorted emails
-            result.push_back(account);
+
+        // Construct result
+        vector<vector<string>> res;
+        for (auto& [root, emails] : rootToEmails) {
+            sort(emails.begin(), emails.end());
+            vector<string> acc = {accounts[root][0]}; // root's name
+            acc.insert(acc.end(), emails.begin(), emails.end());
+            res.push_back(move(acc));
         }
-        
-        return result;
+
+        return res;
     }
 };
