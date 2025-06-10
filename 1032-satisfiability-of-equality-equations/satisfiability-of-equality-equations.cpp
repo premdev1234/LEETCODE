cf4eae916da7dd7bacc5b48d9d217d983a591da4
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-10 17:49:01 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (75.14%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
@@ -1,34 +1,33 @@
 class Solution {
+    vector<char> parents;
+    void _union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ)
+            return;
+        parents[rootP] = rootQ;
+    }
+    bool connected(int p, int q) {
+        return find(p) == find(q);
+    }
+    int find(int x) {
+        if (parents[x] != x)
+            parents[x] = find(parents[x]);
+        return parents[x];
+    }
 public:
-    int find(vector<int>&disjoint,int u){
-        if(disjoint[u]==-1)return u;
-        return disjoint[u]=find(disjoint,disjoint[u]);
-
-    }
     bool equationsPossible(vector<string>& equations) {
-        vector<int>disjoint(26,-1);
-        for(int i=0;i<equations.size();i++)
-        {      
-           
-            if(equations[i][1]=='=')
-            {
-                 int px=find(disjoint,equations[i][0]-'a');
-            int py=find(disjoint,equations[i][3]-'a');
-
-             if(px!=py)disjoint[px]=py;
-                  
-            }
-        }
-          for(int i=0;i<equations.size();i++)
-        {   
-            if(equations[i][1]=='!')
-            {
-                int px=find(disjoint,equations[i][0]-'a');
-                int py=find(disjoint,equations[i][3]-'a');
-                if(px==py)return false;
-
-            }
-    }
-    return true;
+        parents = vector<char>(26);
+        for (int i = 0; i < parents.size(); i++)
+            parents[i] = i;
+        // handles "==" equations first, make union find forest
+        for (auto& e : equations)
+            if (e[1] == '=')
+                _union(e[0] - 'a', e[3] - 'a');
+        // check "!=" equations after
+        for (auto& e : equations)
+            if (e[1] == '!' && connected(e[0] - 'a', e[3] - 'a'))
+                return false;
+        return true;
     }
 };
