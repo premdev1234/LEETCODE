94e21666d84a925fff1089a30d3dccf07a62408f
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-12 15:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (97.22%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
+    int HouseRobberI(vector<int>& nums, int start, int end) {
+        int prev1 = 0, prev2 = 0;
+        for (int i = start; i <= end; ++i) {
+            int curr = max(prev1 + nums[i], prev2);
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
+    }
+
     int rob(vector<int>& nums) {
-        int prev1 =  nums[0];
-        int prev2 = 0;
-        for(int i  = 1 ;  i < nums.size()-1; i++){
-            int curr = max( prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        int ans = prev1;
-        if( nums.size() > 1){
-        prev1 = nums[1];
-        prev2 = 0;
-        for(int i  = 2 ;  i < nums.size(); i++){
-            int curr = max( prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        }
-        ans = max(ans,prev1);
-        return ans;
-        
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+        return max(HouseRobberI(nums, 0, n - 2), HouseRobberI(nums, 1, n - 1));
     }
 };
