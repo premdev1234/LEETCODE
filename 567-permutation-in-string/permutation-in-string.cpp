d2608d1eb0d18d9e149976f876f887cf5d7505b9
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-16 15:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (46.90%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -4,7 +4,7 @@ public:
         if(s1.length() > s2.length()) return false;
         // if(s1.length() == s2.length()) return s1==s2 ;
         int m =  s1.length() , n = s2.length();
-        vector<int> a(26,0) , b(26,0);
+        array<int,26> a = {} , b = {};
         for(int i = 0 ; i< m ;++i){
             a[s1[i]-'a']++;
             b[s2[i]-'a']++;
