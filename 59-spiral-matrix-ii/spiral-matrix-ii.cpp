52589da1619232d4ea527679de889422e6676210
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-16 17:51:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (37.45%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        int top = 0  , left  =0 , bottom =  n-1 , right = n-1 ;
-        vector<vector<int>> a(n,vector<int>(n));
-        int val =  1 ;
-        while(top <= bottom and left <= right){
-            for(int i = left ; i<= right ; i++){
-                a[top][i] = val;
-                val++;    
+        vector<vector<int>> res(n, vector<int>(n, 0));
+
+        int x=0;
+        int y=0;
+        int dx=1;
+        int dy=0;
+
+        for (int i=0;i<n*n;i++){
+            res[y][x]=i+1;
+
+            if(!(0<=x+dx && x+dx < n && 0<=y+dy && y+dy<n && res[y+dy][x+dx]==0)){
+                int temp = dx;
+                dx= -dy;
+                dy = temp;
             }
-            top++;
-            for(int i = top ; i <= bottom ; i++){
-                a[i][right] =  val;
-                val++;
-            }
-            right--;
-            for(int i =  right ; i >= left ; --i){
-                a[bottom][i] =  val;
-                val++;
-            }
-            bottom--;
-            for(int i = bottom ; i >= top ; --i){
-                a[i][left] = val;
-                val++;
-            }
-            left++;
+
+            x=x+dx;
+            y=y+dy;
         }
-        return move(a);
+        
+        return res;
     }
 };
