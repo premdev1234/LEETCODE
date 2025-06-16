2e8e9499cd287c9cb60a6bdcfd250961b33f754a
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-16 12:41:06 +0530

    Time: 121 ms (98.52%) | Memory: 124.1 MB (92.90%) - LeetSync

--- 2012-process-tasks-using-servers/process-tasks-using-servers.cpp
+++ 2012-process-tasks-using-servers/process-tasks-using-servers.cpp
@@ -1,38 +1,29 @@
-#define pq1 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>>
-#define pq2 priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>>
-#define forn(N) for(int i = 0 ; i < (N) ; ++i)
-#define vi vector<int>
-#define SZ(a) a.size()
 class Solution {
 public:
     vector<int> assignTasks(vector<int>& servers, vector<int>& tasks) {
-        pq1 freeServers;
-        pq2 busyServers;
-        forn(SZ(servers))freeServers.emplace(servers[i], i);
-        int time = 0;
-        vi result;
-        forn(SZ(tasks)){
-            time = max(time, i);
-            // Free up any busy servers that are now available
-            while (!busyServers.empty() && get<0>(busyServers.top()) <= time) {
-                auto [availTime, weight, idx] = busyServers.top(); busyServers.pop();
-                freeServers.emplace(weight, idx);
-            }
-
-            if (freeServers.empty()) {
-                // No server is free now — fast-forward time to earliest busy server
-                time = get<0>(busyServers.top());
-                while (!busyServers.empty() && get<0>(busyServers.top()) <= time) {
-                    auto [availTime, weight, idx] = busyServers.top(); busyServers.pop();
-                    freeServers.emplace(weight, idx);
-                }
-            }
-
-            auto [weight, idx] = freeServers.top(); freeServers.pop();
-            result.push_back(idx);
-            busyServers.emplace(time + tasks[i], weight, idx);
+        int n = servers.size();
+        int m = tasks.size();
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> freeServer, endTime;
+        for (int i = 0; i < n; ++i) {
+            freeServer.emplace(servers[i], i);
         }
-
-        return result;
+        int time = 0;
+        vector<int> res(m, 0);
+        for (int i = 0; i < m; ++i) {
+            time = max(time, i);
+            if (freeServer.empty()) {
+                time = endTime.top().first;
+            }
+            while (!endTime.empty() && endTime.top().first <= time) {
+                int freeId = endTime.top().second;
+                endTime.pop();
+                freeServer.emplace(servers[freeId], freeId);
+            }
+            int curId = freeServer.top().second;
+            freeServer.pop();
+            endTime.emplace(time + tasks[i], curId);
+            res[i] = curId;
+        }
+        return res;
     }
 };
