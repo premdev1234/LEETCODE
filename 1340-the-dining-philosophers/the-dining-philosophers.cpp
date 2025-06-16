50674227964f5d462c81492a0bce3604f0812930
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-16 11:20:54 +0530

    Time: 50 ms (9.24%) | Memory: 18.5 MB (62.74%) - LeetSync

--- 1340-the-dining-philosophers/the-dining-philosophers.cpp
+++ 1340-the-dining-philosophers/the-dining-philosophers.cpp
@@ -1,36 +1,9 @@
 class DiningPhilosophers {
+private:
+    mutex mtx[5];
 public:
-    bitset<5> arr;              
-    condition_variable cv;
-    mutex m;
-
-    DiningPhilosophers() {} 
-
-    void wantsToEat(int p,
-                    function<void()> pickLeftFork,
-                    function<void()> pickRightFork,
-                    function<void()> eat,
-                    function<void()> putLeftFork,
-                    function<void()> putRightFork) {
-                        int left = p;
-                        int right = (p + 1)%5;
-        unique_lock<mutex> lock(m);
-        while(!(arr[left] == 0 && arr[right] == 0)) {
-            cv.wait(lock);
-        }
-        arr[left] = 1;
-        arr[right] = 1;
-        pickLeftFork();
-        pickRightFork();
-
-        eat();
-
-        putLeftFork();
-        putRightFork();
-
-        arr[left] = 0;
-        arr[right] = 0;
-        cv.notify_all();
-		
+    void wantsToEat(int philosopher, function<void()> pickLeftFork, function<void()> pickRightFork, function<void()> eat, function<void()> putLeftFork, function<void()> putRightFork) {
+        scoped_lock lck(mtx[philosopher], mtx[(philosopher + 1) % 5]);
+        pickLeftFork(); pickRightFork(); eat(); putLeftFork(); putRightFork();
     }
 };
