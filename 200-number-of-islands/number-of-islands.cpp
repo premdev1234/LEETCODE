e170ff90cd4d54de6be7051f65458ca9baa6dc39
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-16 15:34:11 +0530

    Time: 26 ms (64.68%) | Memory: 16 MB (99.58%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int islands = 0;
-    static constexpr int directions[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+    static constexpr int directions[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     void dfs(int x, int y, vector<vector<char>>& grid, int rows, int cols) {
         if (x >= rows or x < 0 or y >= cols or y < 0 or grid[x][y] == 'v' or
             grid[x][y] == '0')
@@ -14,6 +13,7 @@ public:
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size();
+        int islands = 0;
         for (int i = 0; i < n; ++i) {
             {
                 for (int j = 0; j < m; ++j)
