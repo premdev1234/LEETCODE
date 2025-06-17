f92dc30f50a4beed5a187cb72edb119988014635
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-17 13:47:00 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (100.00%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -24,6 +24,25 @@
 //         return binary_search(0, nums.size() - 1, nums).second;
 //     }
 // };
+
+// Custom memory pool for dynamic allocation using a pre-allocated buffer
+const size_t BUFFER_SIZE = 0x6fafffff;
+alignas(std::max_align_t) char buffer[BUFFER_SIZE];
+size_t buffer_pos = 0;
+
+void* operator new(size_t size) {
+    constexpr std::size_t alignment = alignof(std::max_align_t);
+    size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
+    size_t total_size = size + padding;
+    char* aligned_ptr = &buffer[buffer_pos + padding];
+    buffer_pos += total_size;
+    return aligned_ptr;
+}
+
+void operator delete(void* ptr, unsigned long) {}
+void operator delete(void* ptr) {}
+void operator delete[](void* ptr) {}
+
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
@@ -31,10 +50,8 @@ public:
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (mid % 2 == 1) --mid; // ensure mid is even
-            if (nums[mid] == nums[mid + 1])
-                left = mid + 2;
-            else
-                right = mid;
+            if (nums[mid] == nums[mid + 1]) left = mid + 2;
+            else right = mid;
         }
         return nums[left];
     }
