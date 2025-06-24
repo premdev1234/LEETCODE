7d0a32b85d790c6492ab0825097698f0fee32314
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-24 14:04:19 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (76.60%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n =  nums.size();
-        vector<int> prefix(n) , suffix(n);
-        prefix[0] = suffix[n-1] = 0 ;
-        for(int i = 1 ; i < n ; i++) prefix[i] = prefix[i-1]+nums[i-1];
-        for(int i = n-2 ; i >= 0 ; --i) suffix[i] = suffix[i+1]+nums[i+1];
-        for(int i = 0 ; i < n ; ++i){
-            if(prefix[i] == suffix[i]) return i ;
+        // vector<int> prefix(n) , suffix(n);
+        // prefix[0] = suffix[n-1] = 0 ;
+        // for(int i = 1 ; i < n ; i++) prefix[i] = prefix[i-1]+nums[i-1];
+        // for(int i = n-2 ; i >= 0 ; --i) suffix[i] = suffix[i+1]+nums[i+1];
+        // for(int i = 0 ; i < n ; ++i){
+        //     if(prefix[i] == suffix[i]) return i ;
+        // }
+        // return -1 ;
+        int total =  accumulate(nums.begin(),nums.end(),0);
+        int left =0 , right;
+        for(int i = 0 ; i < n ; i++){
+            right = total - left -nums[i];
+            if(left ==  right) return i ;
+            left += nums[i];
         }
-        return -1 ;
+        return -1;
     }
 };
