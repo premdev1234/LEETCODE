24a5e2d072d022468e714469bfcd888b9b636aaf
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-26 15:18:24 +0530

    Time: 7 ms (7.15%) | Memory: 12.3 MB (9.10%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         if(s.length() != t.length()) return false;
-        vector<vector<int>> s_(129) , t_(129);
+        vector<vector<int>> s_(256) , t_(256);
         for(int i = 0 ;  i< s.length() ; ++i){
             s_[s[i]].push_back(i);
             t_[t[i]].push_back(i);
