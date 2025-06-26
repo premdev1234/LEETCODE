83aeee7a16d3eb4e590ae0993dddc1c2dab78aac
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-26 15:39:00 +0530

    Time: 76 ms (49.34%) | Memory: 93.6 MB (83.94%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -2,15 +2,27 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         // naive bruteforce
-        int n  = nums.size();
-        unordered_map<int,vector<int>> indexes ;
-        for(int i = 0 ; i < n ; ++i){
-            if(indexes.find(nums[i]) != indexes.end()){
-                for(int& idx : indexes[nums[i]]){
-                    if(abs(i-idx) <= k) return true;
-                }
+        // int n  = nums.size();
+        // unordered_map<int,vector<int>> indexes ;
+        // for(int i = 0 ; i < n ; ++i){
+        //     if(indexes.find(nums[i]) != indexes.end()){
+        //         for(int& idx : indexes[nums[i]]){
+        //             if(abs(i-idx) <= k) return true;
+        //         }
+        //     }
+        //     indexes[nums[i]].push_back(i);
+        // }
+        // return false;
+
+        // optimized
+        unordered_set<int> window;
+        for (int i = 0; i < nums.size(); i++) {
+            if (window.count(nums[i]))
+                return true;
+            window.insert(nums[i]);
+            if (window.size() > k) {
+                window.erase(nums[i - k]);
             }
-            indexes[nums[i]].push_back(i);
         }
         return false;
     }
