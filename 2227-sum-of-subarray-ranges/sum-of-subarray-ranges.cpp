f7009c8ef1b92ef0ff4dd2f3815fccce80eea786
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-06-30 15:17:39 +0530

    Time: 2 ms (79.20%) | Memory: 15.7 MB (53.46%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,15 +1,58 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long sum = 0 ;
-        int n =  nums.size();
-        for(int i = 0 ; i< n-1 ; i++){
-            int mx = nums[i] , mn = nums[i];
-            for(int j = i+1 ; j < n ; j++){
-                mx = max(nums[j],mx) , mn =  min(nums[j],mn);
-                sum += (mx-mn) ;
+        int n = nums.size();
+        vector<int> pl(n, -1), nl(n, n), pg(n, -1), ng(n, n);
+        stack<int> mono;
+
+        // Next Less
+        while (!mono.empty()) mono.pop();
+        for (int i = 0; i < n; i++) {
+            while (!mono.empty() && nums[mono.top()] > nums[i]) {
+                nl[mono.top()] = i;
+                mono.pop();
             }
+            mono.push(i);
         }
-        return sum ;
+
+        // Previous Less
+        while (!mono.empty()) mono.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!mono.empty() && nums[mono.top()] >= nums[i]) {
+                pl[mono.top()] = i;
+                mono.pop();
+            }
+            mono.push(i);
+        }
+
+        // Next Greater
+        while (!mono.empty()) mono.pop();
+        for (int i = 0; i < n; i++) {
+            while (!mono.empty() && nums[mono.top()] < nums[i]) {
+                ng[mono.top()] = i;
+                mono.pop();
+            }
+            mono.push(i);
+        }
+
+        // Previous Greater
+        while (!mono.empty()) mono.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!mono.empty() && nums[mono.top()] <= nums[i]) {
+                pg[mono.top()] = i;
+                mono.pop();
+            }
+            mono.push(i);
+        }
+
+        // Contribution
+        long long sum = 0;
+        for (int i = 0; i < n; i++) {
+            long long max_contrib = 1LL * nums[i] * (i - pg[i]) * (ng[i] - i);
+            long long min_contrib = 1LL * nums[i] * (i - pl[i]) * (nl[i] - i);
+            sum += max_contrib - min_contrib;
+        }
+
+        return sum;
     }
 };
