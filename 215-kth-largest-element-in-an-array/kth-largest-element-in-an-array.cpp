84c99329ea27066d404cab3a356e89e61b3a9fe3
PremDev <120601701+premdev1234@users.noreply.github.com>
2025-07-13 13:55:09 +0530

    Time: 3 ms (98.18%) | Memory: 60.4 MB (75.17%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    // int partition(vector<int>&nums , int l , int r){
+    //     int p =  nums[r];
+    //     int i = l ;
+    //     for(int j = l ; j < r ; ++j) if(nums[j] <= p) swap(nums[i++],nums[j]);
+    //     swap(nums[i],nums[r]);
+    //     return i;
+    // }
+    // int qs(vector<int>&nums , int l ,int r , int k){
+    //     if(l == r) return nums[l];
+    //     int p = partition(nums,l,r);
+    //     if(p == k) return nums[k];
+    //     else if(p < k ) return qs(nums,p+1,r,k);
+    //     else return qs(nums,l,p-1,k);
+    // }
     int findKthLargest(vector<int>& nums, int k) {
+        int n  =  nums.size();
+        // return qs(nums,0,n-1,n-k); // kth largest is n-k th smallest 
+        
+        
         int minValue = INT_MAX;
         int maxValue = INT_MIN;
         
