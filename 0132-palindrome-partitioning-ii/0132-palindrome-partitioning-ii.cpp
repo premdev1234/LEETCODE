fa07f18721d557da547aa6bff71826ba776c797f
PremDev <120601701+premdev1234@users.noreply.github.com>
2026-03-07 16:46:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,17 +1,88 @@
 class Solution {
 public:
+    struct Node {
+        int len, link;
+        int next[26];
+        int diff, seriesLink;
+        int seriesAns;
+        Node(int l = 0) {
+            len = l;
+            link = diff = seriesLink = seriesAns = 0;
+            memset(next, 0, sizeof(next));
+        }
+    };
+
     int minCut(string s) {
-        int n = (int)s.size();
-        std::vector<std::vector<bool>> palindrome(n, std::vector<bool>(n,false));
-        for(int i = n-1; ~i; --i)for(int j = i; j < n; ++j)if(s[i] == s[j] && (j - i <= 2 || palindrome[i+1][j-1]))palindrome[i][j] = true; 
-        std::vector<int> dp(n); 
-        for(int i = 0; i < n; ++i){
-            if(palindrome[0][i]) dp[i] = 0;
-            else{
-                dp[i] = i;
-                for(int j = 1; j <= i; ++j)if(palindrome[j][i])dp[i] = min(dp[i], dp[j-1] + 1);
+        int n = s.size();
+        vector<Node> tree(n + 3);
+        vector<int> dp(n + 1, 1e9);
+
+        int last = 2, sz = 2;
+
+        tree[1] = Node(-1);
+        tree[1].link = 1;
+
+        tree[2] = Node(0);
+        tree[2].link = 1;
+
+        dp[0] = -1;
+
+        for (int i = 1; i <= n; i++) {
+
+            int cur = last;
+            int c = s[i-1] - 'a';
+
+            while (true) {
+                int curlen = tree[cur].len;
+                if (i - 2 - curlen >= 0 && s[i-2-curlen] == s[i-1]) break;
+                cur = tree[cur].link;
+            }
+
+            if (!tree[cur].next[c]) {
+
+                tree[++sz] = Node(tree[cur].len + 2);
+                int newnode = sz;
+
+                int link = tree[cur].link;
+
+                while (true) {
+                    int len = tree[link].len;
+                    if (i - 2 - len >= 0 && s[i-2-len] == s[i-1]) break;
+                    link = tree[link].link;
+                }
+
+                tree[newnode].link = tree[link].next[c];
+                if (tree[newnode].link == 0)
+                    tree[newnode].link = 2;
+
+                tree[cur].next[c] = newnode;
+
+                tree[newnode].diff =
+                    tree[newnode].len - tree[tree[newnode].link].len;
+
+                if (tree[newnode].diff ==
+                    tree[tree[newnode].link].diff)
+                    tree[newnode].seriesLink =
+                        tree[tree[newnode].link].seriesLink;
+                else
+                    tree[newnode].seriesLink = tree[newnode].link;
+            }
+
+            last = tree[cur].next[c];
+
+            for (int v = last; tree[v].len > 0; v = tree[v].seriesLink) {
+
+                tree[v].seriesAns =
+                    dp[i - (tree[tree[v].seriesLink].len + tree[v].diff)];
+
+                if (tree[v].diff == tree[tree[v].link].diff)
+                    tree[v].seriesAns = min(tree[v].seriesAns,
+                                            tree[tree[v].link].seriesAns);
+
+                dp[i] = min(dp[i], tree[v].seriesAns + 1);
             }
         }
-        return dp[n-1];
+
+        return dp[n];
     }
 };
