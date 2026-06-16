8e8bfce98926dda49833fc0384a5242d35cd9118
PremDev <120601701+premdev1234@users.noreply.github.com>
2026-06-16 15:38:46 +0530

    1851-MAXIMUM-NUMBER-OF-EVENTS-THAT-CAN-BE-ATTENDED-II Stats: Time: 599 ms (77.91%), Space: 73.4 MB (86.05%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.py
@@ -1,14 +1,13 @@
 class Solution:
-    def maxValue(self, events: List[List[int]], k: int) -> int:
+    def maxValue(self, events, k):
         events.sort()
-        starts = [e[0] for e in events]
         n = len(events)
-        @lru_cache(None)
-        def dfs(i, rem):
-            if i == n or rem == 0:
-                return 0
-            skip = dfs(i + 1, rem)
-            nxt = bisect_right(starts, events[i][1])
-            take = events[i][2] + dfs(nxt, rem - 1)
-            return max(skip, take)
-        return dfs(0, k)
+        starts = [e[0] for e in events]
+        nxt = [0] * n
+        for i in range(n):
+            nxt[i] = bisect_right(starts, events[i][1])
+        dp = [[0] * (k + 1) for _ in range(n + 1)]
+        for i in range(n - 1, -1, -1):
+            for j in range(1, k + 1):
+                dp[i][j] = max( dp[i + 1][j], events[i][2] + dp[nxt[i]][j - 1] )
+        return dp[0][k]
