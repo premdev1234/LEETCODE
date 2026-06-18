252335598ee975a411bdd82b34891fcca3595c5f
PremDev <120601701+premdev1234@users.noreply.github.com>
2026-06-18 13:46:37 +0530

    0678-VALID-PARENTHESIS-STRING Stats: Time: 0 ms (100%), Space: 7.9 MB (93%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    bool dfs(int idx, int balance, string &s) {
-        if(balance < 0) return false;
-        if(idx == s.size())return balance == 0;
-        if(dp[idx][balance] != -1)return dp[idx][balance];
-        bool ans = false;
-        if(s[idx] == '(')ans = dfs(idx + 1, balance + 1, s);
-        else if(s[idx] == ')') ans = dfs(idx + 1, balance - 1, s);
-        else ans = dfs(idx + 1, balance + 1, s) || dfs(idx + 1, balance - 1, s) || dfs(idx + 1, balance, s); 
-        return dp[idx][balance] = ans;
-    }
     bool checkValidString(string s) {
-        int n = s.size();
-        dp.assign(n + 1, vector<int>(n + 1, -1));
-        return dfs(0, 0, s);
+        int minbal=0,maxbal=0;
+        for(char &x:s){
+            if(x=='(') minbal++ ,  maxbal++;
+            else if(x==')') minbal-- , maxbal--;
+            else minbal-- , maxbal++;
+            if(minbal<0) minbal=0;
+            if(maxbal<0) return false;
+        }
+        return minbal==0;
     }
 };
